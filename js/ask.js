d4b13fb2c11f6fdb2ceb8eb958e59b630afb8b3f
RckyEK <ricky820617@gmail.com>
2015-01-14 19:35:41 +0800

    part of ask

---
+++ js/ask.js
@@ -0,0 +1,18 @@
+
+window.onload = function() {
+   
+$(document).ready(function() {
+                        var l='<div class="left">'+
+                            '<div class="dhead">'+
+                                '<img src="img/dog.jpg"/>'+
+                            '安安'+
+                            '</div>'+
+
+                        '</div>';
+   $('#lefth').append(l);
+   // alert("0.0");
+       });
+
+
+}
+
